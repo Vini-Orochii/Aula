3a2992482d7ed624950a600854c474e96c0a8f53
Vini-Orochii <viniventura655@gmail.com>
2022-05-17 12:25:01 -0300

    comit 1

---
+++ projeto/src/components/ItemLista.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ItemLista = ({nome}) => {
+    return ( <>
+        
+        <li>{nome}</li>   
+    </> );
+}
+
+export default ItemLista;
---
+++ projeto/src/views/Tela.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Lista from '../components/Lista';
+import "./css/tela.css"
+
+const Tela = () => {
+    return ( <>
+    <h1>Minha Lista</h1>
+    <Lista />
+    </>);
+}
+
+export default Tela;
